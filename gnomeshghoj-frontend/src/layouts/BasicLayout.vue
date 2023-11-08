1a701162b19fe2db2d03a4e343f5497e6f8704b2
gnomeshgh2 <1051671645@qq.com>
2023-11-08 22:18:55 +0800

    前端初始化

---
+++ gnomeshghoj-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 1300px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"
+        ><a href="https:github.com" target="_blank"
+          >gnomeshghOj by gnomeshgh</a
+        ></a-layout-footer
+      >
+    </a-layout>
+  </div>
+
+  <router-view />
+</template>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: #42b983;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #42b983;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
